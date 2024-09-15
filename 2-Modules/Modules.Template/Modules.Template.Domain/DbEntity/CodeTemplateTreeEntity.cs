a2c2e0fd627bbc404258c633c2ec57ecea037868
NET <NET@DESKTOP-0IS12J5>
2024-09-15 23:08:18 +0800

    修改代码生成器

--- 2-Modules/Modules.Template/Modules.Template.Domain/DbEntity/CodeTemplateTreeEntity.cs
+++ 2-Modules/Modules.Template/Modules.Template.Domain/DbEntity/CodeTemplateTreeEntity.cs
@@ -3,31 +3,8 @@
 namespace Modules.Template.Domain.DbEntity
 {
     [SugarTable("CodeTemplate")]
-    public class CodeTemplateTreeEntity
+    public class CodeTemplateTreeEntity : Core.Domain.Base.TreeEntity<CodeTemplateTreeEntity>
     {
-        #region 树结构默认列
-
-        [SugarColumn(IsPrimaryKey = true, Length = 64, ColumnDescription = "ID")]
-        public string id { get; set; }
-
-        [SugarColumn(Length = 32, IsNullable = true, DefaultValue = "0", ColumnDescription = "父ID")]
-        public string parentid { get; set; }
-
-        [SugarColumn(IsNullable = true, ColumnDescription = "更新时间")]
-        public DateTime? updatetime { get; set; }
-
-        [SugarColumn(ColumnDescription = "建立时间")]
-        public DateTime createtime { get; set; }
-
-        [SugarColumn(Length = 64, IsNullable = true, ColumnDescription = "更新用户")]
-        public string updateuid { get; set; }
-
-        [SugarColumn(Length = 64, ColumnDescription = "建立用户")]
-        public string createuid { get; set; }
-
-        [SqlSugar.SugarColumn(IsIgnore = true)]
-        public List<CodeTemplateTreeEntity> SqlsugarTreeChild { get; set; }//用于映射sqlsugar查询得到的树状结构,注意添加IsIgnore = true
-        #endregion
 
         [SugarColumn(IsNullable = true)]
         public string username { get; set; }
@@ -37,5 +14,6 @@ namespace Modules.Template.Domain.DbEntity
 
         [SugarColumn(IsNullable = true)]
         public string notes { get; set; }
+
     }
 }
